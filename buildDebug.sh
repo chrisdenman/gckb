199244654f30bf8c5d582c71b399b53070de0fb6
Chris Denman <chrisdenman@icloud.com>
2025-07-19 08:00:56 +0100

    Bash scripts for building debug and release binaries.
    Fixes #19

---
+++ buildDebug.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -d ".github" ]; then
+  echo "'./.github' directory does not exist, are you in the home directory?" >&2
+  exit 1
+fi
+
+BUILD_TYPE="Debug"
+BUILD_DIR="build/cmake-build-${BUILD_TYPE,,}"
+mkdir -p "${BUILD_DIR}"
+cmake -S . -B "${BUILD_DIR}" -G Ninja -DCMAKE_BUILD_TYPE="${BUILD_TYPE}"
+cmake --build "${BUILD_DIR}" --target all --clean-first  -j 12
