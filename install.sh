2921cc6359efd2ebb3a8e46ee97135f10b7e837b
Chris Denman <chrisdenman@icloud.com>
2025-07-29 11:18:20 +0100

    Missing build dependencies are prompted for, also:
      - install.sh checks for .git directory now, not .github
      - updated version number to 0.0.7
      - improved the README.md (docs/inline commands to run scripts)

--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-if [ ! -d ".github" ]; then
-  echo "'./.github' directory does not exist, are you in the home directory?" >&2
-  exit 1
-fi
-
 readonly BUILD_TREE="build"
 readonly SOURCE_TREE="."
 readonly GENERATOR="Ninja"
+
+if [ ! -d ".git" ]; then
+  echo "'./.git' directory does not exist, are you in the home directory?" >&2
+  exit 1
+fi
+
+# Check the required dependencies are installed
+for cmd in "cmake" "pkgconf"; do
+  &>/dev/null which "${cmd}" \
+    || >&2 echo "${cmd} is not installed, please install it using \"sudo apt-get install ${cmd}\""
+done;
+dpkg-query --show --showformat='${binary:Package}\n' | grep --quiet --regexp "libglib2\..-dev.*" \
+  || >&2 echo "libglib2.0-dev is not installed, please install with \"sudo apt-get install libglib2.0-dev\""
 
 # Generate a Project Buildsystem : cmake [<options>] -B <path-to-build> [-S <path-to-source>]
 cmake -Wdev --fresh -B "${BUILD_TREE}" -S "${SOURCE_TREE}" -G "${GENERATOR}" -DCMAKE_BUILD_TYPE="Release"
